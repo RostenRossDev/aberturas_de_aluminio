cecea2fe1353ac947adea9b08e7aab16b674616d
Costantini Nestor <costantini.nestor.m@gmail.com>
2024-08-03 01:47:21 -0300

    add carousel

--- src/main/resources/static/js/navbar.js
+++ src/main/resources/static/js/navbar.js
@@ -1,32 +1,8 @@
-//document.addEventListener("DOMContentLoaded", function() {
-//    var navbarToggler = document.querySelector('.navbar-toggler');
-//    var navbarCollapse = document.querySelector('#navbarNav');
-//
-//    if (navbarToggler && navbarCollapse) {
-//        navbarToggler.addEventListener('click', function() {
-//            navbarCollapse.classList.toggle('show');
-//        });
-//    }
-//});
+// content box
+document.addEventListener("DOMContentLoaded", function() {
+            setTimeout(function() {
+                document.getElementById('contentBox').classList.add('visible');
+            }, 1);
+        });
 
-//document.addEventListener("DOMContentLoaded", function() {
-//    var navbarToggler = document.querySelector('.navbar-toggler');
-//    var navbarCollapse = document.querySelector('#navbarNav');
-//
-//    if (navbarToggler && navbarCollapse) {
-//        navbarToggler.addEventListener('click', function() {
-//            var isExpanded = navbarToggler.getAttribute('aria-expanded') === 'true';
-//            console.log("aria expanded: " + isExpanded);
-//            navbarToggler.setAttribute('aria-expanded', !isExpanded);
-//            if (isExpanded) {
-//                console.log("remover show");
-//                navbarCollapse.classList.remove('show');
-//                navbarCollapse.classList.add('collapse');
-//            } else {
-//                console.log("remover collapse");
-//                navbarCollapse.classList.add('show');
-//                navbarCollapse.classList.remove('collapse');
-//            }
-//        });
-//    }
-//});
+// SILEDER
